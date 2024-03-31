77bc44dfb3fe9562bca9ff61f1ef055e75874949
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-03-30 23:47:02 -0400

    Icons.hx updated.

--- KEHUD/scripts/Icons.hx
+++ KEHUD/scripts/Icons.hx
@@ -1,12 +1,12 @@
-var fuckedIconBops:Bool = true;
+var kadeIconBops:Bool = true;
 
 function onUpdatePost(elapsed:Float) {
-  if (!fuckedIconBops) {return;}
+  if (!kadeIconBops) {return;}
   for (i in 0...2) {
     var icon:FlxSprite = i == 0 ? game.iconP1 : game.iconP2;
     if (icon == null) {continue;}
     icon.scale.x = 1;
-    icon.origin.x = 50 + (i == 1 ? 30 : 0);
+    icon.origin.x = 50 + (30 * i);
     icon.origin.y = 0;
   }
   return;
