b23eac51ddc25fe4cd63b155ec93a7c916756bde
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-07-09 00:30:21 -0400

    Update Input.hx

--- KEHUD/scripts/Input.hx
+++ KEHUD/scripts/Input.hx
@@ -1,5 +1,3 @@
-// not recommended to edit but meh
-
 import openfl.events.KeyboardEvent;
 import psychlua.LuaUtils.Function_Stop as funcStop;
 
@@ -25,115 +23,64 @@ function sortNotesByPriority(a:Note, b:Note):Int {
 }
 
 function input(event:KeyboardEvent):Void {
+  final lastTime:Float = Conductor.songPosition;
   final key:Int = getKey(game.keysArray, event.keyCode);
+  final k:String = game.keysArray[key];
+  final strum:StrumNote = game.playerStrums.members[key];
   if (!game.controls.controllerMode && (key > -1 || key < game.playerStrums.length)) {
-    final inputFromArray:String = game.keysArray[key];
-    if (game.controls.justPressed(inputFromArray)) {
-      inputPress(key);
-    } else if (game.controls.justReleased(inputFromArray)) {
-      inputRelease(key);
+    if (game.controls.justPressed(k)) {
+      if ((!game.inCutscene && !game.paused && !game.endingSong && !game.cpuControlled && game.generatedMusic && game.startedCountdown) &&
+      game.callOnScripts('onKeyPressPre', [key]) != funcStop) {
+        if (!game.startingSong || lastTime >= 0) {
+          Conductor.songPosition = FlxG.sound.music.time;
+        }
+        var sortedNotesList:Array<Note> = game.notes.members.filter(function(a:Note):Bool {
+          return a != null && a.noteData == key && a.mustPress && a.canBeHit && !a.tooLate &&
+          !a.wasGoodHit && !a.missed && !a.hitByOpponent && !a.noteWasHit && a.spawned && !a.blockHit && !a.isSustainNote;
+        });
+        if (sortedNotesList.length != 0 && game.strumsBlocked[key]) {
+          sortedNotesList.sort(sortNotesByPriority);
+          game.goodNoteHit(sortedNotesList[sortedNotesList.length - 1]);
+          sortedNotesList = [];
+        } else if (sortedNotesList.length == 0) {
+          game.callOnScripts('onGhostTap', [key]);
+          if (!ClientPrefs.data.ghostTapping) {
+            game.noteMissPress(key);
+          }
+        }
+        if (!game.keysPressed.contains(key)) {
+          game.keysPressed.push(key);
+        }
+        if (strum != null && strum.animation.curAnim.name != 'confirm') {
+          strum.playAnim('pressed');
+          strum.resetAnim = 0;
+        }
+        Conductor.songPosition = lastTime;
+        game.callOnScripts('onKeyPress', [key]);
+      }
+    } else if (game.controls.justReleased(k)) {
+      if ((!game.inCutscene && !game.paused && !game.endingSong) &&
+      game.callOnScripts('onKeyReleasePre', [key]) != funcStop) {
+        if (game.guitarHeroSustains) {
+          var sustainTbl:Array<Note> = game.notes.members.filter(function(a:Note):Bool {
+            return a != null && a.noteData == key && a.mustPress && !a.tooLate && !a.missed &&
+            !a.hitByOpponent && a.spawned && !a.blockHit && a.isSustainNote && a.parent.wasGoodHit && !a.wasGoodHit;
+          });
+          if (sustainTbl.length != 0) {
+            sustainTbl.sort(simpleSort);
+            game.noteMiss(sustainTbl[0]);
+            sustainTbl = [];
+          }
+        }
+        if (strum != null && strum.animation.curAnim.name != 'static') {
+          strum.playAnim('static');
+          strum.resetAnim = 0;
+        }
+        game.callOnScripts('onKeyRelease', [key]);
+      }
     }
   }
   return;
-}
-
-function inputPress(key:Int):Dynamic {
-
-  if ((game.inCutscene || game.paused || game.endingSong || game.cpuControlled || !game.generatedMusic || !game.startedCountdown) ||
-  game.callOnScripts('onKeyPressPre', [key]) == funcStop) {
-    return null;
-  }
-
-  final lastTime:Float = Conductor.songPosition;
-  if (!game.startingSong || lastTime >= 0) {
-    Conductor.songPosition = FlxG.sound.music.time;
-  }
-
-  var sortedNotesList:Array<Note> = game.notes.members.filter(function(a:Note):Bool {
-    return a != null && !game.strumsBlocked[a.noteData] && a.noteData == key && a.mustPress && a.canBeHit && !a.tooLate &&
-    !a.wasGoodHit && !a.missed && !a.hitByOpponent && !a.noteWasHit && a.spawned && !a.blockHit && !a.isSustainNote;
-  });
-
-  if (sortedNotesList.length != 0) {
-    sortedNotesList.sort(sortNotesByPriority);
-    final coolNote:Note = sortedNotesList[sortedNotesList.length - 1];
-    if (sortedNotesList.length > 1) {
-      for (epicNote in sortedNotesList) {
-        if (epicNote != coolNote && epicNote.strumTime - coolNote.strumTime <= 12.5 * 100 / Conductor.bpm) {
-          if (game.guitarHeroSustains && epicNote.tail.length != 0) {
-            for (tail in epicNote.tail) {
-              if (tail != null) {
-                game.invalidateNote(tail);
-              }
-            }
-          }
-          game.invalidateNote(epicNote);
-        }
-      }
-    }
-    game.goodNoteHit(coolNote);
-    sortedNotesList = [];
-  } else {
-    game.callOnScripts('onGhostTap', [key]);
-    if (!ClientPrefs.data.ghostTapping) {
-      game.noteMissPress(key);
-    }
-  }
-
-  if (!game.keysPressed.contains(key)) {
-    game.keysPressed.push(key);
-  }
-
-  final strum:StrumNote = game.playerStrums.members[key];
-  if (strum != null && strum.animation.curAnim.name != 'confirm') {
-    strum.playAnim('pressed');
-    strum.resetAnim = 0;
-  }
-
-  Conductor.songPosition = lastTime;
-
-  return game.callOnScripts('onKeyPress', [key]);
-
-}
-
-function inputRelease(key:Int):Dynamic {
-
-  if ((game.inCutscene || game.paused || game.endingSong) || game.callOnScripts('onKeyReleasePre', [key]) == funcStop) {
-    return null;
-  }
-
-  if (game.guitarHeroSustains) {
-
-    var sustainTbl:Array<Note> = game.notes.members.filter(function(a:Note):Bool {
-      return a != null && a.noteData == key && a.mustPress && !a.tooLate && !a.missed &&
-      !a.hitByOpponent && a.spawned && !a.blockHit && a.isSustainNote && a.parent.wasGoodHit && !a.wasGoodHit;
-    });
-
-    if (sustainTbl.length != 0) {
-      sustainTbl.sort(simpleSort);
-      final tail:Note = sustainTbl[0];
-      game.noteMiss(tail);
-      for (t in tail.parent.tail) {
-        if (t != null) {
-          t.alpha = .35;
-          t.multAlpha = .6;
-        }
-      }
-      tail.missed = tail.ignoreNote = tail.tooLate = true;
-      tail.canBeHit = false;
-      sustainTbl = [];
-    }
-
-  }
-
-  final strum:StrumNote = game.playerStrums.members[key];
-  if (strum != null && strum.animation.curAnim.name != 'static') {
-    strum.playAnim('static');
-    strum.resetAnim = 0;
-  }
-
-  return game.callOnScripts('onKeyRelease', [key]);
-
 }
 
 function onCreatePost():Void {
